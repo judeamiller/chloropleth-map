1c75ba21d7a670dd3f84a50009d4dfec6a0d7812
Jude Baca-Miller <judeamiller@gmail.com>
2018-10-12 17:09:42 -0600

    initial commit of map.js file.  state outlines shown.

---
+++ map.js
@@ -0,0 +1,8 @@
+var map = L.map('mapid').setView([37.8, -96], 4);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
+    id: 'mapbox.light',
+    // attribution: ...
+}).addTo(map);
+
+L.geoJson(statesData).addTo(map);
